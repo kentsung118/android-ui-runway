a81bf6820b7e9151c0c4d77a60efda32e830ca77
kentsong <song046@gmail.com>
2021-01-04 17:09:39 +0800

    searchPosition 左右 testCase 完成

---
+++ slimsample/src/main/java/com/kent/android/slim/sample/letv/desktopmanager/Direction.kt
@@ -0,0 +1,8 @@
+package com.kent.android.slim.sample.letv.desktopmanager
+
+public enum class Direction {
+    LEFT,
+    RIGHT,
+    UP
+
+}
--- slimsample/src/test/java/com/kent/android/slim/sample/letv/desktopmanager/TestItemKtTest.kt
+++ slimsample/src/test/java/com/kent/android/slim/sample/letv/desktopmanager/TestItemKtTest.kt
@@ -1,6 +1,7 @@
 package com.kent.android.slim.sample.letv.desktopmanager
 
 import com.kent.android.slim.sample.letv.desktopmanager.bean.ScreenInfo
+import org.junit.Assert
 import org.junit.Assert.assertArrayEquals
 import org.junit.Before
 import org.junit.Test
@@ -10,17 +11,18 @@ import org.junit.Test
  */
 class TestItemKtTest {
 
-    var testItem = TestItem()
+    var testItem = TestItem(7)
 
     @Before
     fun init() {
     }
 
-    class ScreenItem(pkgName: String, val locked: Boolean= false) : ScreenInfo(){
+    class ScreenItem(pkgName: String, val locked: Boolean = false) : ScreenInfo() {
         init {
             super.setPackageName(pkgName)
             super.setLocked(locked)
         }
+
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (javaClass != other?.javaClass) return false
@@ -35,6 +37,32 @@ class TestItemKtTest {
 
     fun screenItem(pkgName: String, locked: Boolean = false): ScreenInfo {
         return ScreenItem(pkgName, locked)
+    }
+
+    /**
+     * 帮助建构初始数据
+     * ＠Param count 几笔数据
+     * ＠Param lockPos 锁的位置 (可多笔)
+     */
+    private fun initData(count: Int, lockPos: Array<Int>): ArrayList<ScreenInfo> {
+        var data = ArrayList<ScreenInfo>()
+        for (i in 1..count) {
+            data.add(screenItem("A" + i))
+        }
+        for (i in lockPos) {
+            data[i].locked = true
+        }
+        return data
+    }
+
+    @Test
+    fun test_InitData() {
+        val data = initData(20, arrayOf(0, 5, 6))
+        Assert.assertEquals(20, data.size)
+        Assert.assertEquals(data[0].locked, true)
+        Assert.assertEquals(data[5].locked, true)
+        Assert.assertEquals(data[6].locked, true)
+        Assert.assertEquals(data[7].locked, false)
     }
 
     @Test
@@ -164,5 +192,79 @@ class TestItemKtTest {
                 )
         )
     }
+
+    @Test
+    fun keyLeft_normal() {
+        //左移
+        val r1 = testItem.searchPosition(mutableListOf(
+                screenItem("A"),
+                screenItem("B"),
+                screenItem("C"),
+                screenItem("D"),
+                screenItem("E")) as ArrayList<ScreenInfo>, 3, Direction.LEFT)
+        Assert.assertEquals(2, r1)
+
+        //左移边界
+        val r2 = testItem.searchPosition(mutableListOf(
+                screenItem("A"),
+                screenItem("B"),
+                screenItem("C"),
+                screenItem("D"),
+                screenItem("E"),
+                screenItem("F"),
+                screenItem("G")) as ArrayList<ScreenInfo>, 7, Direction.LEFT)
+        Assert.assertEquals(-1, r2)
+
+    }
+
+    @Test
+    fun keyLeft_withLock() {
+        //有锁的情况
+        val r3 = testItem.searchPosition(initData(4, lockPos = arrayOf(2)), 3, Direction.LEFT)
+        Assert.assertEquals(1, r3)
+
+        //有锁*2的情况
+        val r4 = testItem.searchPosition(initData(4, lockPos = arrayOf(1, 2)), 3, Direction.LEFT)
+        Assert.assertEquals(0, r4)
+
+        //有锁、边界情况
+        val r5 = testItem.searchPosition(initData(9, lockPos = arrayOf(7)), 8, Direction.LEFT)
+        Assert.assertEquals(-1, r5)
+    }
+
+    @Test
+    fun keyRight_normal() {
+        //右移
+        val r1 = testItem.searchPosition(initData(4, lockPos = arrayOf()), 2, Direction.RIGHT)
+        Assert.assertEquals(3, r1)
+
+        //右移边界
+        val r3 = testItem.searchPosition(initData(8, lockPos = arrayOf()), 7, Direction.RIGHT)
+        Assert.assertEquals(-1, r3)
+    }
+
+    @Test
+    fun keyRight_withLock() {
+        //右移有锁
+        val r2 = testItem.searchPosition(initData(7, lockPos = arrayOf(3)), 2, Direction.RIGHT)
+        Assert.assertEquals(4, r2)
+
+        //右移有锁*2
+        val r3 = testItem.searchPosition(initData(7, lockPos = arrayOf(3,4)), 2, Direction.RIGHT)
+        Assert.assertEquals(5, r3)
+
+        //有锁、边界情况
+        val r5 = testItem.searchPosition(initData(9, lockPos = arrayOf(7)), 6, Direction.RIGHT)
+        Assert.assertEquals(-1, r5)
+    }
+
+    @Test
+    fun keyUp_normal() {
+        //上移
+        val r1 = testItem.searchPosition(initData(7, lockPos = arrayOf(3)), 2, Direction.UP)
+        Assert.assertEquals(4, r1)
+    }
+
+
 }
 
